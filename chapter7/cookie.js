c9556fbc60360885b7a4dd0c68eecd4347e4125c
find_qin <find_qin@foxmail.com>
2019-02-08 09:05:01 +0800

    U7:Connect自带的中间件

---
+++ chapter7/cookie.js
@@ -0,0 +1,8 @@
+var connect = require('connect');
+var cookieParser = require('cookie-parser');
+var app = connect()
+  .use(cookieParser('tobi is a cool ferret'))
+  .use(function(req, res) {
+    console.log(req.cookies);
+    console.log(req.signedCookies);
+  }).listen(3000);
